f5b5a543c271f927354cff18dd24a3568d44382e
Malaquia24 <arthur.matheus0607@gmail.com>
2023-08-31 08:14:50 -0300

    upload

---
+++ Projeto/classe.py
@@ -0,0 +1,9 @@
+class ToDoList:
+    def adicionar_tarefa(self, descricao: str):
+        pass
+
+    def excluir_tarefa(self, indice: int):
+        pass
+
+    def listar_tarefas(self):
+        pass
