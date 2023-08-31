9210ac2733ab294388bc823fadd28ef73c3c70ad
Malaquia24 <arthur.matheus0607@gmail.com>
2023-08-31 08:41:29 -0300

    update

--- Projeto/algoritmo.py
+++ Projeto/algoritmo.py
@@ -1,3 +1,39 @@
 from classe import *
+import os
 def main():
-    pass
+    objeto = ToDoList()
+    print("Bem vindo ao ToDoList")
+    menu = True
+    while menu == True:
+        try:
+            deseja = input("O que deseja fazer: \n[1]Adicionar \n[2]Excluir \n[3]Listar \n[4]Sair \n-")
+            os.system("cls")
+
+            match deseja:
+                case "1":
+                    descri = input("Digite a tarefa: ")
+                    objeto.adicionar_tarefa(descri)
+                    os.system("pause")
+                    os.system("cls")
+                case "2":
+                    index = int(input("Qual é o numero da tarfe que deseja excluir \n-"))
+                    objeto.excluir_tarefa(index)
+                    os.system("pause")
+                    os.system("cls")
+                case "3":
+                    objeto.listar_tarefas()
+                    os.system("pause")
+                    os.system("cls")
+                case  "4":
+                    menu = False
+                case '_':
+                    print("Opção inválida")
+                    os.system("pause")
+                    os.system("cls")
+
+
+        except Exception as erro:
+            print("Valor inválido")
+            print(erro.__class__.__name__)
+            os.system("pause")
+            os.system("cls")
--- Projeto/classe.py
+++ Projeto/classe.py
@@ -1,9 +1,18 @@
 class ToDoList:
+    def __init__(self):
+        self.lista = []
+
     def adicionar_tarefa(self, descricao: str):
-        pass
+        self.lista.append(descricao)
+        print("Tarefa adicionada")
+        
 
     def excluir_tarefa(self, indice: int):
-        pass
+        self.lista.pop(indice - 1)
+        print("Tarefa excluida")
 
     def listar_tarefas(self):
-        pass
+        n = 0
+        for i in self.lista:
+            n += 1
+            print(f"{n}. {i}")
